03992282a64afdaeec04f23d2c1940521126a5b3
BRUNO OLIVEIRA <brunnomoraaes@gmail.com>
2022-09-13 15:11:11 -0300

    segundo commit

--- scritps.js
+++ scritps.js
@@ -1,32 +1,47 @@
 let dados = [
-   {
-     id: 1,
-     name: "Bertie Yates",
-     email: "Sincere@april.biz",
-     image: "https://robohash.org/1?set=set2",
-   },
-   {
-     id: 2,
-     name: "Hester Hogan",
-     email: "Shanna@melissa.tv",
-     image: "https://robohash.org/2?set=set2",
-   },
-   {
-     id: 3,
-     name: "Larry Little",
-     email: "Nathan@yesenia.net",
-     image: "https://robohash.org/3?set=set2",
-   },
-   {
-     id: 4,
-     name: "Sean Walsh",
-     email: "Julianne.OConner@kory.org",
-     image: "https://robohash.org/4?set=set2",
-   },
-   {
-     id: 5,
-     name: "Lola Gardner",
-     email: "Lucio_Hettinger@annie.ca",
-     image: "https://robohash.org/5?set=set2",
-   },
- ];
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    email: 'Sincere@april.biz',
+    image: 'https://robohash.org/1?set=set2'
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    email: 'Shanna@melissa.tv',
+    image: 'https://robohash.org/2?set=set2'
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    email: 'Nathan@yesenia.net',
+    image: 'https://robohash.org/3?set=set2'
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    email: 'Julianne.OConner@kory.org',
+    image: 'https://robohash.org/4?set=set2'
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    email: 'Lucio_Hettinger@annie.ca',
+    image: 'https://robohash.org/5?set=set2'
+  }
+]
+
+let elementH1 = document.querySelector('h1')
+
+for (let item of dados) {
+  let elementArticle = document.createElement('article')
+  elementArticle.classList.add('monstros')
+  elementArticle.innerHTML = `
+    <img src="https://robohash.org/NFF.png?set=set2&size=150x150" alt="avatar" />
+    <div>
+    <h2>nome</h2>
+    <p>e-mail</p>
+    </div>
+    `
+  elementH1.insertAdjacentElement('afterend', elementArticle)
+}

931cd706ea8c441192cd65ae8451e6540745ea0e
BRUNO OLIVEIRA <brunnomoraaes@gmail.com>
2022-09-13 17:36:33 -0300

    commit async await

--- scritps.js
+++ scritps.js
@@ -4,37 +4,39 @@ let buttonLimpar = document.querySelector('.btn-azul')
 elementH1.innerText = `${dados.length} monstros`
 
 fetch('https://jsonplaceholder.typicode.com/users', {
-  method: 'GET',
+  method: 'GET'
+}).then(response => {
+  return response.json()
 })
-  .then((response) => {
-    return response.json()
-  })
-  .then((json) => {
-    dados = json.slice(0, 5);
-    elementH1.innerText = `${dados.length} monstros`
 
-    for (let objects of dados) {
-      let elementArticle = document.createElement('article')
-      elementArticle.classList.add('monstros')
-      elementArticle.innerHTML = `
+buttonLimpar.addEventListener('click', () => {
+  let articles = document.querySelectorAll('article')
+  for (let i = 0; i < articles.length; i++) {
+    articles[i].remove()
+  }
+  dados = []
+  elementH1.innerText = `${dados.length} monstros`
+})
+
+async function renderizar() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const dadosJson = await response.json()
+
+  dados = dadosJson.slice(0, 5)
+  elementH1.innerText = `${dados.length} monstros`
+
+  for (let objects of dados) {
+    let elementArticle = document.createElement('article')
+    elementArticle.classList.add('monstros')
+    elementArticle.innerHTML = `
       <img src="https://robohash.org/${objects.id}?set=set2" alt="avatar" />
       <div>
       <h2>${objects.name}</h2>
       <p>${objects.email}</p>
         </div>
         `
-      elementH1.insertAdjacentElement('afterend', elementArticle)
-    }
-
-})
-
-
-
-buttonLimpar.addEventListener('click', () => {
-  let articles = document.querySelectorAll('article')
-  for (let i = 0; i < articles.length; i++) {
-    articles[i].remove();
+    elementH1.insertAdjacentElement('afterend', elementArticle)
   }
-  dados = [];
-  elementH1.innerText = `${dados.length} monstros`
-})
+}
+
+renderizar()
